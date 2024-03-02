1a2a3b874d8f921dec6d460c726d5b16ac09e001
Jorge Q. <jqb94@hotmail.com>
2024-03-02 03:14:40 +0100

    Filters are included in the main website

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -6,7 +6,46 @@ const Anuncio = require('../models/Anuncio');
 router.get('/', async function(req, res, next) {
 
   try {
-    const anuncios = await Anuncio.find();
+    const filterByTag = req.query.tags;
+    const filterByVenta = req.query.venta;
+    const filterMinPrecio = req.query.minPrecio;
+    const filterMaxPrecio = req.query.maxPrecio;
+    const filterNombreStartWith = req.query.nombreStartWith;
+
+    const skip = req.query.skip;
+    const limit = req.query.limit;
+    const sort = req.query.sort;
+    const fields = req.query.fields;
+
+    const filter = {};
+
+    // filtro por tag
+    if (filterByTag) {
+      filter.tags = {'$all': filterByTag };
+      console.log(filterByTag);
+    }
+
+    // filtro por venta
+    if (filterByVenta) {
+      filter.venta = filterByVenta;
+    }
+
+    // filtro por precio
+    if (filterMinPrecio && filterMaxPrecio) {
+      filter.precio =  {'$gte': filterMinPrecio, '$lte': filterMaxPrecio};
+    } else if (filterMaxPrecio) {
+      filter.precio = {'$lte': filterMaxPrecio};
+    } else if (filterMinPrecio) {
+      filter.precio =  {'$gte': filterMinPrecio};
+    }
+
+    // filtro por comienzo de nombre
+    if (filterNombreStartWith) {
+      filter.nombre = {'$regex': '^' + filterNombreStartWith, $options: 'i'};
+    }
+
+    const anuncios = await Anuncio.listar(filter, skip, limit, sort, fields);
+
     res.locals.anuncios = anuncios;
   } catch (error) {
     next(error)
