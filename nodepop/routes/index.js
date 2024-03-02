7c5ec8f72009aef198ed9326e29ba187b066a0e6
Jorge Q. <jqb94@hotmail.com>
2024-03-02 01:52:33 +0100

    Adding main website including list of products

--- nodepop/routes/index.js
+++ nodepop/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+const Anuncio = require('../models/Anuncio');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+
+  try {
+    const anuncios = await Anuncio.find();
+    res.locals.anuncios = anuncios;
+  } catch (error) {
+    next(error)
+  }
+
+  res.render('index', { title: 'Anuncios', imagesPath: '/images/' });
 });
 
 module.exports = router;
